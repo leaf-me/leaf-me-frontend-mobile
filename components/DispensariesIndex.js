34922ff92c5f6d940a76d96755d8880f4c87b238
PESolut <jahaadpetty@pursuit.org>
2024-04-17 18:20:39 -0400

    successfully styled disp. index

--- components/DispensariesIndex.js
+++ components/DispensariesIndex.js
@@ -2,47 +2,50 @@ import React from 'react';
 import { View, FlatList } from 'react-native';
 import { useDisProvider } from '../Providers/DispensariesProvider.js';
 import { useContextProvider } from '../Providers/Provider.js';
-import { Text, Image } from 'react-native';
+import { Text, Image, Dimensions } from 'react-native';
 import { v4 as uuidv4 } from "uuid";
 import styles from './DispensariesIndexStyles.js'; // Import the StyleSheet
 import disp1Img from '../assets/dis1.png'
 import disp2Img from '../assets/dis2.png'
+import disp3Img from '../assets/dis3.png'
 
 
 // helper function to check if given string is the correct image, then returns the correct image
 // for use to display correct dispensary image based on the key value for image key
 // takes in imageFileName as first param
 // takes in the import of each image as the second and third arguments
-const findImageThenRender = (imageFileName, disp1Img, disp2Img) => {
+const findImageThenRender = (imageFileName, disp1Img, disp2Img, disp3Img) => {
     if(imageFileName === 'image1.jpg'){
         return disp1Img
     } else if (imageFileName === 'image2.jpg'){
         return disp2Img
     } else {
-        return null
+        return disp3Img
     }
 }
 
 const DispensariesIndex = () => {
+    const screenWidth = Dimensions.get('window').width;
+
     const { dispensaries } = useDisProvider() 
 
     console.log('data present',dispensaries)
 
-
+    // dispensaryItem Component:
     const Item = ({name, address, deliveryfee, image}) => {
 
-        const imageSrc = findImageThenRender(image, disp1Img, disp2Img)
+        const imageSrc = findImageThenRender(image, disp1Img, disp2Img, disp3Img)
 
 
         return (
         <View style={styles.item}>
             <Image
-            style={{ height: 300, width: 300}}
+            style={{ height: 225, width: screenWidth-5, borderRadius: 20}}
             source={imageSrc}
             />
             <Text style={styles.title}>{name}</Text>
-            <Text style={styles.title}>{address}</Text>
-            <Text style={styles.title}>{deliveryfee}</Text>
+            <Text style={styles.address}>{address}</Text>
+            <Text style={styles.price}>${deliveryfee} Delivery Fee</Text>
         </View>
         )
         
--- components/DispensariesIndexStyles.js
+++ components/DispensariesIndexStyles.js
@@ -2,10 +2,22 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     item: {
-        backgroundColor: "blue",
+        backgroundColor: "white",
         padding: 2,
         margin: 2,
+        borderRadius: 20,
       },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        letterSpacing: 2,
+    },
+    address: {
+        textAlign: 'right',
+    },
+    price: {
+        marginTop: -20
+    }
   });
   
   export default styles;
