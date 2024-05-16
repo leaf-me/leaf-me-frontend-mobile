bc2e0e3b64fd269ceca65707ce59775533efa811
PESolut <jahaadpetty@pursuit.org>
2024-05-16 08:38:11 -0400

    render basketIndex onto basket.js

--- views/Basket.js
+++ views/Basket.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
+import BasketIndex from '../components/BasketIndex';
 
 const Basket = () => {
+
     return (
+        
         <View>
             
         </View>
