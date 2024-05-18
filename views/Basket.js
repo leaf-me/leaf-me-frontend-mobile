cf8a0c6986910bcb6605bdd102fc8755e3d843c5
PESolut <jahaadpetty@pursuit.org>
2024-05-18 09:15:14 -0400

    pre add to basket functionality; TICKET COMPLETE

--- views/Basket.js
+++ views/Basket.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { View } from 'react-native';
 import BasketIndex from '../components/BasketIndex';
-import { useUserContext } from '../Providers/UserProvider.js';
 import Header from '../components/Header';
 import NavBar from '../components/NavBar';
 
 
 const Basket = () => {
-    const { basketItems } = useUserContext()
 
+    // create add to cart button comp
+    // on press, retrieve state of all of basketItems,
+
+    // seperate each order by like dispensary_id's
+    // iterate over basketItems;
+        // on each el 'order', put like dispensary_id's in its own unique order object
+        // go until theres no more orders
+        // create order state with this data:
+        /*
+        total
+        status
+        client_user_id
+        */
+    // post each order to the order table
+    // populate each order with related storeItems onto orderstoreitems table
 
     return (
         
