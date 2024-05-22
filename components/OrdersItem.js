8272140386c9c91bb3e8992e3564cfac12adb6f8
PESolut <jahaadpetty@pursuit.org>
2024-05-22 15:20:04 -0400

    ticket complete - rendering out data based on orders status

---
+++ components/OrdersItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View , Text } from 'react-native';
+
+const ordersItem = () => {
+    return (
+        <View>
+            <Text>here</Text>
+            
+        </View>
+    );
+};
+
+export default ordersItem;
--- views/Orders.js
+++ views/Orders.js
@@ -1,17 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
+import { View, FlatList, Text } from 'react-native';
 import { useContextProvider } from '../Providers/Provider';
 import { getUsersOrders } from '../components/Functions.js'
+import OrdersItem from '../components/ordersItem.js';
 
 const Orders = () => {
     const { userID } = useContextProvider()
-    const [userOrders, setUserOrders] = useState([])
+    const [completeOrders, setCompleteOrders] = useState(null)
+    const [pendingOrders, setPendingOrders] = useState(null)
+
+    /* EXAMPLE ORDER OBJ
+     {"client_user_id": 1, "dispensary_id": 1, "id": 66, "status": "NEW", "total": "541.47"}
+    */
 
     // Helpers
     const fetchUsersOrders = async (userID) => {
         const orders = await getUsersOrders(userID)
-        setUserOrders(orders)
-        // console.log('userOrders:',userOrders)
+        // now sort
+        sortOrdersByCompleteOrOther(orders)
+    }
+    const renderItem = ({item}) => {
+        return (
+            <OrdersItem clientUserID={item.client_user_id} dispensaryID={item.dispenary_id} itemID={item.id} status={item.status} total={item.total} />
+        )
+    }
+    const sortOrdersByCompleteOrOther = (orders) => {
+        const tempCompleteOrders = orders.filter((order)=> order.status === "COMPLETE" )
+        const tempPendingOrders = orders.filter((order)=> order.status !== "COMPLETE")
+        console.log('\nsortOrdersByCompleteOrOther\ncomplete Orders:\n',tempCompleteOrders,'\nPending Orders:\n',tempPendingOrders)
+        setCompleteOrders(tempCompleteOrders)
+        setPendingOrders(tempPendingOrders)
+
+
     }
 
     // Hooks
@@ -23,7 +43,26 @@ const Orders = () => {
 
     return (
         <View>
-            
+            <Text>Pending Orders</Text>
+            {
+                pendingOrders ?
+                <FlatList
+                data={pendingOrders}
+                renderItem={renderItem}
+                keyExtractor={(pendingOrder)=> pendingOrder.id }
+                />
+                : <Text>None</Text>
+            }
+            <Text>Complete Orders</Text>
+            {
+                completeOrders ?
+                <FlatList
+                data={completeOrders}
+                renderItem={renderItem}
+                keyExtractor={(completeOrder)=> completeOrder.id }
+                />
+                : <Text>None</Text>
+            }
         </View>
     );
 };
